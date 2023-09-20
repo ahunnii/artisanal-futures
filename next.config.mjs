9352549e1307129a3afca5e2c1daf2632bc5712b
Andrew Hunn <ahunn59@gmail.com>
2023-09-20 02:59:45 -0400

    (chore) disable linting for now

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,11 @@ const config = {
     // !! WARN !!
     ignoreBuildErrors: true,
   },
-
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
    *
