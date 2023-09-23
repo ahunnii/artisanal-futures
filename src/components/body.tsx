ffb95f2eabac2688e0cd021183cd6e822cdbf22e
Andrew Hunn <ahunn59@gmail.com>
2023-09-22 21:43:50 -0400

    (chore) fix loading logic for Body

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -1,15 +1,15 @@
 import { useSession } from "next-auth/react";
+import { Suspense, useEffect, useState } from "react";
 import Container from "~/components/ui/container";
 import Footer from "./footer";
 import Navbar from "./navbar";
 import PageLoader from "./ui/page-loader";
-
 const Body = ({ children }: { children: React.ReactNode }) => {
-  const { data: session } = useSession();
+  const [loading, setLoading] = useState(true);
+
   return (
-    <main className="flex min-h-screen flex-col">
-      {!session && <PageLoader />}
-      {session && (
+    <>
+      <main className="flex min-h-screen flex-col">
         <>
           <Navbar />
           <Container className=" flex h-full flex-grow flex-col items-stretch p-8">
@@ -17,8 +17,8 @@ const Body = ({ children }: { children: React.ReactNode }) => {
           </Container>
           <Footer />
         </>
-      )}
-    </main>
+      </main>
+    </>
   );
 };
 export default Body;
