b6b26b5629da8f4635f8523dc24770727e7f73b2
Andrew Hunn <ahunn59@gmail.com>
2023-11-29 23:02:26 -0500

    (chore) modify height values of containers

--- src/layouts/route-layout.tsx
+++ src/layouts/route-layout.tsx
@@ -5,7 +5,7 @@ const RouteLayout = ({ children }: { children: React.ReactNode }) => {
     <main className="fixed flex h-screen w-full flex-col ">
       <Navbar />
       <div
-        className="md:p-08 s relative flex  h-[calc(100vh-64px)]  flex-col-reverse  bg-slate-50 p-2  max-md:overflow-auto md:flex-row
+        className="  relative flex  flex-grow flex-col-reverse bg-slate-50  p-2  max-md:overflow-auto md:flex-row  lg:h-[calc(100vh-64px)] lg:flex-none
       "
       >
         {children}
