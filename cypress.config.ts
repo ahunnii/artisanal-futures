34d7194a9dda9dc33f5ceaef6efb0964769bed93
Kwame Porter Robinson <kporterrobinson@gmail.com>
2023-12-21 16:26:27 -0500

    (wip) issue getting past Auth0 login, see issue

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from "cypress";
+const path = require('path')
+const dotenv = require('dotenv')
+const envLocal = dotenv.config({ path: path.resolve(process.cwd(), '.env') }).parsed;
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'http://localhost:3000',
     experimentalStudio: true
   },
+  env: {
+    ...envLocal
+  }
 });
+
+console.log(envLocal);
--- cypress/e2e/my_first_test.cy.ts
+++ cypress/e2e/my_first_test.cy.ts
@@ -1,19 +1,19 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://www.artisanalfutures.org/')
+  it('can log in with csdt dev account', () => {
+    cy.visit('/password-protect');
+
+    console.log('Password:', Cypress.env('NEXT_PUBLIC_PASSWORD_PROTECT'));
+
+    cy.get('input[name=password]').type(Cypress.env('NEXT_PUBLIC_PASSWORD_PROTECT') + '{enter}')
+    
+    // Start Auth0 flow
+    cy.contains('Sign in with email').click();
+    cy.origin('https://artisanalfutures-dev.us.auth0.com', () => {
+      cy.get('input[name=username]').type(Cypress.env('USERNAME'))
+      cy.get('input[name=password]').type(Cypress.env('PASSWORD'))
+      cy.get('input[name=password]').type('{enter}')
+
   })
-  it('create new transaction', () => {
-    /* ==== Generated with Cypress Studio ==== */
-    cy.visit('www.artisanalfutures.org');
-    /* ==== End Cypress Studio ==== */
-    /* ==== Generated with Cypress Studio ==== */
-    cy.get('.my-6 > .flex > .mx-auto').click();
-    cy.get('.mt-5 > .rounded-md').click();
 
-    cy.get(
-      ':nth-child(1) > .bg-slate-50 > .items-center > .gap-2 > .bg-slate-500 > .lucide > [d="M22 7v3a2 2 0 0 1-2 2v0a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 16 12a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 12 12a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 8 12a2.7 2.7 0 0 1-1.59-.63.7.7 0 0 0-.82 0A2.7 2.7 0 0 1 4 12v0a2 2 0 0 1-2-2V7"]'
-    ).click();
-
-    /* ==== End Cypress Studio ==== */
   })  
 })
