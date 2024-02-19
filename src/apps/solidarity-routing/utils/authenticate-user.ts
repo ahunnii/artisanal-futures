3d981f9e732aa82a644f1af3a0641c3f51d7e0fb
Andrew Hunn <ahunn59@gmail.com>
2024-02-19 15:59:31 -0500

    (fix) update routing serverside auth function to return something in all cases

--- src/apps/solidarity-routing/utils/authenticate-user.ts
+++ src/apps/solidarity-routing/utils/authenticate-user.ts
@@ -1,67 +1,43 @@
 import type { GetServerSidePropsContext } from "next";
 import { prisma } from "~/server/db";
 import {
-  authenticateSession,
   authenticateUser,
   redirectIfNoSession,
   redirectIfUnauthorized,
 } from "~/utils/auth";
-
-// const authenticateUser = async (ctx: GetServerSidePropsContext) => {
-//   const session = (await authenticateSession(ctx)) as Session;
-//   const user = session;
-//   console.log(user);
-//   // if (!session.user) return redirectIfNoSession(ctx.resolvedUrl);
-//   if (!session.user) return null;
-//   return session.user;
-// };
 
 export const authenticateRoutingServerSide = async (
   ctx: GetServerSidePropsContext,
   allowForDriver?: boolean,
   conditional?: (ctx: GetServerSidePropsContext) => unknown
 ) => {
-  const token = ctx.req.cookies?.verifiedDriver;
-  if (allowForDriver && token)
+  const driverCookie = ctx.req.cookies?.verifiedDriver;
+  if (allowForDriver)
+    return { props: { verifiedDriver: driverCookie !== undefined } };
+
+  const user = await authenticateUser(ctx);
+
+  if (user?.role === "DRIVER") return redirectIfUnauthorized();
+  if (!user) return redirectIfNoSession(ctx.resolvedUrl);
+
+  const query = ctx.query;
+  const depot = await prisma.depot.findUnique({
+    where: { id: query.depotId as string },
+  });
+
+  if (!depot)
     return {
-      props: {
-        verifiedDriver: true,
+      redirect: {
+        destination: "/tools/solidarity-pathways",
+        permanent: false,
       },
     };
 
-  if (allowForDriver && !token)
-    return {
-      props: {
-        verifiedDriver: false,
-      },
-    };
+  const isOwner = user.id === depot.ownerId;
 
-  if (!allowForDriver) {
-    const user = await authenticateUser(ctx);
+  if (!isOwner) return redirectIfUnauthorized();
 
-    const query = ctx.query;
+  if (conditional) return conditional(ctx);
 
-    if (user?.role === "DRIVER") return redirectIfUnauthorized();
-    if (!user) return redirectIfNoSession(ctx.resolvedUrl);
-
-    const depot = await prisma.depot.findUnique({
-      where: { id: query.depotId as string },
-    });
-
-    if (!depot)
-      return {
-        redirect: {
-          destination: "/tools/solidarity-pathways",
-          permanent: false,
-        },
-      };
-
-    const isOwner = user.id === depot.ownerId;
-
-    if (!isOwner) return redirectIfUnauthorized();
-
-    if (conditional) return conditional(ctx);
-
-    return { props: {} };
-  }
+  return { props: {} };
 };
