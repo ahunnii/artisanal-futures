ef360add667dad0dbc1574169cf6da236ccccbec
Andrew Hunn <ahunn59@gmail.com>
2024-03-07 19:51:29 -0500

    (chore) add ignore linting to config -- need to remove before merging with production

--- next.config.mjs
+++ next.config.mjs
@@ -29,7 +29,11 @@ const config = {
   typescript: {
     ignoreBuildErrors: true,
   },
-
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
    *
